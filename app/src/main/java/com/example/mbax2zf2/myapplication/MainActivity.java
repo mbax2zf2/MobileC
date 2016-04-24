9a19689ab23dc7b6451556e5453098b35586a8ee
Zhengquan Fan <mbax2zf2@Zhengquans-iMac.local>
2016-04-24 15:51:56 +0100

    Project Created

---
+++ app/src/main/java/com/example/mbax2zf2/myapplication/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.mbax2zf2.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public EditText ipText;
+    public EditText portText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ipText = (EditText) findViewById(R.id.ipView);
+        portText = (EditText) findViewById(R.id.portView);
+        ipText.setOnClickListener(this);
+        portText.setOnClickListener(this);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        System.out.println("Clicked"+ v);
+        if(v == ipText && ipText.getText().equals("Enter server IP"))
+            ipText.setText("");
+        if(v == portText && portText.getText().equals("Enter port"))
+            portText.setText("");
+    }
+}
