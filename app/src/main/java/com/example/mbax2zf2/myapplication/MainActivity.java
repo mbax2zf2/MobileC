f91b504615244b7bee78235111037a18f67ae6f9
Forsaken <mbax2zf2@Forsakens-MacBook-Air-2.local>
2016-04-24 20:21:26 +0100

    Client Socket Completed.

--- app/src/main/java/com/example/mbax2zf2/myapplication/MainActivity.java
+++ app/src/main/java/com/example/mbax2zf2/myapplication/MainActivity.java
@@ -1,8 +1,7 @@
 package com.example.mbax2zf2.myapplication;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -10,10 +9,21 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     public EditText ipText;
     public EditText portText;
+    public String iptxt;
+    public String  porttxt;
+    public Socket sock;
 
 
     @Override
@@ -24,6 +34,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         portText = (EditText) findViewById(R.id.portView);
         ipText.setOnClickListener(this);
         portText.setOnClickListener(this);
+        iptxt = ipText.getText().toString();
+        porttxt = portText.getText().toString();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
@@ -54,9 +66,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v)
     {
         System.out.println(ipText.getText());
-        if(R.id.ipView == v.getId())
+        if(R.id.ipView == v.getId() && ipText.getText().toString().equals(iptxt))
             ipText.setText("");
-        if(v == portText && portText.getText().equals("Enter port"))
+        if(R.id.portView == v.getId() && portText.getText().toString().equals(porttxt))
             portText.setText("");
+    }
+
+    public void sendMsg(View v)
+    {
+        System.out.println("Button Clicked");
+        String server_addr=ipText.getText().toString();
+        int port = Integer.parseInt(portText.getText().toString());
+        new clientSockets(server_addr,port).execute();
+
+    }
+
+    class clientSockets extends AsyncTask<Void, Void, String> {
+        private String ip;
+        private int portNo;
+
+        public clientSockets(String ipAddr, int port)
+        {
+            super();
+            this.ip=ipAddr;
+            this.portNo=port;
+        }
+
+        @Override
+        protected String doInBackground(Void... params){
+            String response = "";
+            try {
+                DatagramSocket clientSocket = new DatagramSocket();
+                InetAddress IPAddress = InetAddress.getByName(ip);
+                byte[] sendData = new byte[1024];
+                byte[] receiveData = new byte[1024];
+                String sentence = "NACK1";
+                sendData = sentence.getBytes();
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, portNo);
+                clientSocket.send(sendPacket);
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                clientSocket.receive(receivePacket);
+                response = new String(receivePacket.getData());
+                System.out.println("FROM SERVER:" + response);
+                clientSocket.close();
+            }
+            catch (Exception e){
+                System.err.println(e);
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String resp){
+            System.out.println("Task finished");
+        }
     }
 }
