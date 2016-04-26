d80591345b6a4475240ddf8842c133f461987f6a
Zhengquan Fan <mbax2zf2@Zhengquans-iMac.local>
2016-04-26 09:41:53 +0100

    Task 1 finished. With UI update

--- app/src/main/java/com/example/mbax2zf2/myapplication/MainActivity.java
+++ app/src/main/java/com/example/mbax2zf2/myapplication/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
@@ -77,19 +78,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         System.out.println("Button Clicked");
         String server_addr=ipText.getText().toString();
         int port = Integer.parseInt(portText.getText().toString());
-        new clientSockets(server_addr,port).execute();
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        String msg = String.valueOf(spinner.getSelectedItem());
+        new clientSockets(server_addr,port,msg).execute();
+
 
     }
 
     class clientSockets extends AsyncTask<Void, Void, String> {
         private String ip;
         private int portNo;
+        private String sentence;
 
-        public clientSockets(String ipAddr, int port)
+        public clientSockets(String ipAddr, int port, String msg)
         {
             super();
             this.ip=ipAddr;
             this.portNo=port;
+            this.sentence=msg;
         }
 
         @Override
@@ -98,15 +104,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             try {
                 DatagramSocket clientSocket = new DatagramSocket();
                 InetAddress IPAddress = InetAddress.getByName(ip);
-                byte[] sendData = new byte[1024];
+                byte[] sendData;
                 byte[] receiveData = new byte[1024];
-                String sentence = "NACK1";
                 sendData = sentence.getBytes();
                 DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, portNo);
                 clientSocket.send(sendPacket);
+                clientSocket.setSoTimeout(3000);
                 DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
                 clientSocket.receive(receivePacket);
-                response = new String(receivePacket.getData());
+                byte[] data = new byte[receivePacket.getLength()];
+                System.arraycopy(receivePacket.getData(), receivePacket.getOffset(), data, 0, receivePacket.getLength());
+                response = new String(data);
                 System.out.println("FROM SERVER:" + response);
                 clientSocket.close();
             }
@@ -119,6 +127,52 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         @Override
         protected void onPostExecute(String resp){
             System.out.println("Task finished");
+            EditText opt = (EditText) findViewById(R.id.optText);
+            byte[] data = resp.getBytes();
+            byte crc = CRC8(data);
+            int c = crc & 0xFF;
+            opt.setText("");
+            opt.append("ASCII string received: " + resp + "\n");
+            opt.append("CRC of original string: " + data[data.length-1] + "\n");
+            opt.append("CRC of received string without CRC: " + c + "\n");
+        }
+
+        protected byte CRC8(byte[] data){
+            byte crc = data[data.length-1];
+            byte generatorRemainder = 0b00000111;    //First bit is omitted
+            // A popular variant complements remainderPolynomial here
+            byte remainderPolynomial = 0x00;
+            for (int i=0; i< data.length-1; i++) {
+                remainderPolynomial = (byte) (data[i]^remainderPolynomial);
+                for(int j=0; j<8; j++)
+                {    //  8 bits per byte
+                    if ((remainderPolynomial&0b10000000)!=0) {
+                        remainderPolynomial  = (byte)((remainderPolynomial << 1)^generatorRemainder);
+                    } else {
+                        remainderPolynomial  = (byte)(remainderPolynomial << 1);
+                    }
+                }
+            }
+            // A popular variant complements remainderPolynomial here
+            return remainderPolynomial;
+        }
+
+
+        public void deci2bin(int d, int size, int []b)
+        {
+            b[size-1] = d&0x01;
+            for (int i = size - 2; i >= 0; i--) {
+                d = d >> 1;
+                b[i] = d & 0x01;
+            }
+        }
+
+        public int bin2deci(int []b, int size)
+        {
+            int i, d=0;
+            for (i = 0; i < size; i++)
+                d += b[i] << (size - i - 1);
+            return(d);
         }
     }
 }
